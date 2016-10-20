11a16a933d97b3200df444e29d09f5e2cc003545
Dylan McArthur <dylanmcarthur@Alex.local>
2016-10-20 01:17:09 -0700

    Node uses subclasses instead of enum type to describe purpose

--- Cue/Node.swift
+++ Cue/Node.swift
@@ -6,49 +6,26 @@
 //  Copyright © 2016 Dylan McArthur. All rights reserved.
 //
 
-import Foundation
-
-public enum CueNodeType {
-	// Block
-	case document
-	case scene
-	case comment
-	case cue(String)
-	case description
-	// Inline
-	case italic
-}
-
-public class CueNode: CustomStringConvertible {
+public class Node {
 	
-	public var type: CueNodeType
-	public var location: Int
-	public var length: Int
-	public var children = [CueNode]()
-	public var markingRanges = [(Int, Int)]()
+	public var text = [UInt8]()
+	public var location = 0
+	public var length = 0
+	public var markingRanges = [(Int,Int)]()
 	
-	public init(type: CueNodeType, location: Int, length: Int) {
-		self.type = type
-		self.location = location
-		self.length = length
+	public var parent: Node?
+	public var children = [Node]()
+	
+	func addChild(_ child: Node) {
+		//	TODO: if parent can't accept this child, back up until we find a parent that can
+		var par = self
+		
+		child.parent = par
+		par.children.append(child)
 	}
-	
-	func addChild(node: CueNode) {
-		children.append(node)
-	}
-	
-	func addChildren(nodes: [CueNode]) {
-		children.append(contentsOf: nodes)
-	}
-	
-	func addMarkingRange(range: (Int, Int)) {
-		markingRanges.append(range)
-	}
-	
-	// MARK: Iterative Functions (FOR EXISTING TREES ONLY)
 	
 	// Binary search on first level of children
-	public func search(index: Int) -> CueNode? {
+	public func search(index: Int) -> Node? {
 		var lower = 0
 		var upper = children.count
 		
@@ -58,7 +35,6 @@ public class CueNode: CustomStringConvertible {
 			
 			if index >= midChild.location && index <= midChild.location+midChild.length {
 				if let found = midChild.search(index: index) {
-					print("found")
 					return found
 				}
 				
@@ -72,13 +48,35 @@ public class CueNode: CustomStringConvertible {
 		
 		return nil
 	}
-	
-	public var description: String {
-		var desc = "\(type)(\(location),\(length))"
-		if !children.isEmpty {
-			desc += "{ " + children.map { $0.description }.joined(separator: ", ") + " } "
-		}
-		return desc
-	}
-	
 }
+
+public class Block: Node { }
+public class Inline: Node { }
+
+protocol Nameable {
+	var name: [UInt8] { get set }
+}
+
+public class Document: Block {}
+
+public class Heading: Block, Nameable {
+	public var name: [UInt8] = []
+}
+public class ActHeading: Heading {}
+public class ChapterHeading: Heading {}
+public class SceneHeading: Heading {}
+
+public class Description: Block {}
+public class CueBlock: Block {}
+public class Lyric: Block {}	// Even though lyrics are parsed as inlines, we consider lyrics a block element
+public class Comment: Block {}
+
+public class Cue: Block, Nameable {
+	public var name: [UInt8] = []
+}
+public class RegularCue: Cue {}
+public class DualCue: Cue {}
+
+public class Text: Inline {}
+public class Emphasis: Inline {}
+public class Reference: Inline {}
