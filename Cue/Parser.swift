090abc4b856d808ec98dc36980b63e5267cfbdd5
Dylan McArthur <dmcarth@users.noreply.github.com>
2016-10-21 10:37:54 -0700

    Added some Text blocks

--- Cue/Parser.swift
+++ Cue/Parser.swift
@@ -11,23 +11,27 @@ class CueParser {
 	private var root = Document()
 	private var currentNode: Node
 	private var currentIndex = 0
+	private var endOfLineIndex = 0
 	private var lineNumber = 0
 	private var offset = 0
 	private var firstNonSpace = 0
 	private var blank = false
-	private var currentLine = ArraySlice<UInt8>()
+	private var currentLine = ArraySlice<UInt16>()
+	private var data = [UInt16]()
 	private var lastLineLength = 0
 	
 	init() {
 		currentNode = self.root
 	}
 	
-	func process(_ buffer: [UInt8]) {
+	func process(_ buffer: [UInt16]) {
+		data = buffer
+		
 		// First process lines
 		let end = buffer.count
 		while currentIndex < end {
 			// Find line ending index
-			var endOfLineIndex = currentIndex
+			endOfLineIndex = currentIndex
 			while endOfLineIndex < end {
 				if scanForLineEnding(buffer[endOfLineIndex]) {
 					endOfLineIndex += 1
@@ -44,28 +48,23 @@ class CueParser {
 	}
 	
 	func finish() -> Node {
-		// if any text remains that hasn't been processed, process it as one line
-		
 		// Finalize everything in the tree
-		//		while !(currentNode === root) {
-		//			currentNode = finalize(currentNode)
-		//		}
-		//		finalize(root)
+		finalize(root)
 		
-		// processInlines()
+		processInlines()
 		
-		// consolidateTextNodesToRoot
+		root.length = currentIndex
 		
 		return root
 	}
 	
-	private func process(line: ArraySlice<UInt8>) {
+	private func process(line: ArraySlice<UInt16>) {
 		// validate utf8? either way, set current line to line
 		currentLine = line
 		
 		// Ensure line ends with newline
 		if line.count == 0 || !scanForLineEnding(currentLine.last!) {
-			currentLine.append(0x0a)	// '\n'
+			currentLine.append(0x000a)	// '\n'
 		}
 		
 		offset = 0
@@ -75,61 +74,111 @@ class CueParser {
 		lineNumber += 1
 		
 		var block = blockForCurrentInput()
+		block.location = currentIndex
+		block.length = endOfLineIndex-currentIndex
+		
 		let cont = container(for: &block)
 		cont.addChild(block)
+		cont.location = min(cont.location, block.location)
+		if cont.location+cont.length < block.location+block.length {
+			cont.length = block.location + block.length - cont.location
+		}
+		
+		if let cb = block as? Cue {
+			scanFirstNonspace()
+			
+			if scanLyricPrefix() {
+				let ly = Lyric()
+				ly.location = currentIndex+firstNonSpace
+				ly.length = endOfLineIndex - ly.location
+				cb.addChild(ly)
+				let del = Delimiter()
+				del.location = ly.location
+				del.length = 1
+				ly.addChild(del)
+				let te = Text()
+				te.location = currentIndex+firstNonSpace+1
+				te.length = endOfLineIndex - te.location
+				cb.addChild(te)
+			} else {
+				let te = Text()
+				te.location = currentIndex+firstNonSpace
+				te.length = endOfLineIndex - te.location
+				cb.addChild(te)
+			}
+		} else {
+			scanFirstNonspace()
+			
+			let te = Text()
+			te.location = currentIndex+firstNonSpace
+			te.length = endOfLineIndex-te.location
+			block.addChild(te)
+		}
 	}
 	
-	private func finalize(_ node: Node) -> Node {
-		return node.parent!
+	private func finalize(_ node: Node) {
+//		node.enumerate { (leaf) in
+//			if leaf is Block {
+//				let start = (leaf.children.last != nil) ?  (leaf.children.last!.location+leaf.children.last!.length) : leaf.location
+//				
+//				let te = Text()
+//				te.location = start
+//				te.length = leaf.location+leaf.length-te.location
+//				leaf.addChild(te)
+//			}
+//		}
 	}
 	
 	// MARK: Parsing Blocks
 	
 	private func blockForCurrentInput() -> Block {
-		//refactor to include text parts
 		scanFirstNonspace()
 		
 		if scanActHeading() {
 			let ah = ActHeading()
-//			ah.name = Array(currentLine[firstNonSpace+4..<currentLine.endIndex])
 			return ah
 		}
 		if scanChapterHeading() {
 			let ch = ChapterHeading()
-//			ch.name = Array(currentLine[firstNonSpace+8..<currentLine.endIndex])
 			return ch
 		}
 		if scanSceneHeading() {
 			let sh = SceneHeading()
-//			sh.name = Array(currentLine[firstNonSpace+6..<currentLine.endIndex])
 			return sh
 		}
 		if scanComment() {
 			let c = Comment()
+			let del = Delimiter()
+			del.location = firstNonSpace+currentIndex
+			del.length = 2
+			c.addChild(del)
 			offset += 2
-//			c.text = Array(currentLine[offset..<currentLine.endIndex])
 			return c
 		}
 		if scanLyricPrefix() {
 			let l = Lyric()
-//			l.text = Array(currentLine[firstNonSpace+1..<currentLine.endIndex])
+			let del = Delimiter()
+			del.location = firstNonSpace+currentIndex
+			del.length = 1
+			l.addChild(del)
 			return l
 		}
 		if scanDualPrefix() {
 			if let dist = scanCueName() {
 				let dc = DualCue()
-//				dc.name = Array(currentLine[firstNonSpace..<dist])
+				dc.addName(ofLength: dist, atIndex: firstNonSpace+currentIndex)
+				offset = 1+dist+firstNonSpace
 				return dc
 			}
 		}
 		if let dist = scanCueName() {
 			let rc = RegularCue()
-//			rc.name = Array(currentLine[firstNonSpace..<dist])
+			rc.addName(ofLength: dist, atIndex: firstNonSpace+currentIndex)
+			offset = 1+dist+firstNonSpace
 			return rc
 		}
 		
 		let des = Description()
-//		des.text = currentLine
 		return des
 	}
 	
@@ -143,6 +192,8 @@ class CueParser {
 		// A regular cue is always level-2, with it's own initial parent cueBlock
 		case is RegularCue:
 			let cueBlockContainer = CueBlock()
+			cueBlockContainer.location = currentIndex
+			cueBlockContainer.length = endOfLineIndex-currentIndex
 			container.addChild(cueBlockContainer)
 			return cueBlockContainer
 		default:
@@ -174,14 +225,44 @@ class CueParser {
 		}
 		
 		block = Description()
-//		block.text = currentLine
 		return self.root
+	}
+	
+	// MARK: Parsing Inlines
+	
+	func processInlines() {
+		
+		root.enumerate { (leaf) in
+			guard leaf is Text else { return }
+			
+			var stack = [(String, Int)]()
+			
+			var i = leaf.location
+			var buff = [UInt16]()
+			while i < leaf.location+leaf.length {
+				let c = data[i]
+				
+				if c == 0x002a {
+					stack.append(("*", i))
+				}
+				else if c == 0x005b {
+					stack.append(("[", i))
+				}
+				else if c == 0x005d {	// ']'
+					
+				}
+				
+				
+				i += 1
+			}
+		}
+		
 	}
 	
 	// MARK: Scanners
 	
-	func scanForLineEnding(_ c: UInt8) -> Bool {
-		return c == 0x0a || c == 0x0d // '\n', '\r'
+	func scanForLineEnding(_ c: UInt16) -> Bool {
+		return c == 0x000a || c == 0x000d // '\n', '\r'
 	}
 	
 	func scanFirstNonspace() {
@@ -190,7 +271,7 @@ class CueParser {
 		while firstNonSpace < currentLine.count {
 			let c = currentLine[currentIndex+firstNonSpace]
 			
-			if c == 0x20 || c == 0x09 { // ' ', '\t'
+			if c == 0x0020 || c == 0x0009 { // ' ', '\t'
 				firstNonSpace += 1
 			} else {
 				break
@@ -201,8 +282,8 @@ class CueParser {
 	}
 	
 	func scanComment() -> Bool {
-		if currentLine.count > (firstNonSpace+1) && currentLine[currentIndex+firstNonSpace] == 0x2f {
-			if currentLine[currentIndex+firstNonSpace+1] == 0x2f {	// '/'
+		if currentLine.count > (firstNonSpace+1) && currentLine[currentIndex+firstNonSpace] == 0x002f {
+			if currentLine[currentIndex+firstNonSpace+1] == 0x002f {	// '/'
 				return true
 			}
 		}
@@ -210,7 +291,7 @@ class CueParser {
 	}
 	
 	func scanLyricPrefix() -> Bool {
-		if currentLine[currentIndex+firstNonSpace] == 0x7e { // '~'
+		if currentLine[currentIndex+firstNonSpace] == 0x007e { // '~'
 			return true
 		}
 		return false
@@ -222,7 +303,7 @@ class CueParser {
 		if currentLine.count < firstNonSpace+4 {
 			return false
 		}
-		if (currentLine[currentIndex+firstNonSpace] == 0x53 || currentLine[currentIndex+firstNonSpace] == 0x73) && currentLine[currentIndex+firstNonSpace+1] == 0x63 && currentLine[currentIndex+firstNonSpace+2] == 0x74 && currentLine[currentIndex+firstNonSpace+3] == 0x20 { // 'A'||'a', 'c', 't', ' '
+		if (currentLine[currentIndex+firstNonSpace] == 0x0053 || currentLine[currentIndex+firstNonSpace] == 0x0073) && currentLine[currentIndex+firstNonSpace+1] == 0x0063 && currentLine[currentIndex+firstNonSpace+2] == 0x0074 && currentLine[currentIndex+firstNonSpace+3] == 0x0020 { // 'A'||'a', 'c', 't', ' '
 			return true
 		}
 		return false
@@ -232,7 +313,7 @@ class CueParser {
 		if currentLine.count < firstNonSpace+8 {
 			return false
 		}
-		if (currentLine[currentIndex+firstNonSpace] == 0x43 || currentLine[currentIndex+firstNonSpace] == 0x63) && currentLine[currentIndex+firstNonSpace+1] == 0x68 && currentLine[currentIndex+firstNonSpace+2] == 0x61 && currentLine[currentIndex+firstNonSpace+3] == 0x70 && currentLine[currentIndex+firstNonSpace+4] == 0x74 && currentLine[currentIndex+firstNonSpace+5] == 0x65 && currentLine[currentIndex+firstNonSpace+6] == 0x72 && currentLine[currentIndex+firstNonSpace+7] == 0x20 { // 'C'||'c', 'h', 'a', 'p', 't', 'e', 'r', ' '
+		if (currentLine[currentIndex+firstNonSpace] == 0x0043 || currentLine[currentIndex+firstNonSpace] == 0x0063) && currentLine[currentIndex+firstNonSpace+1] == 0x0068 && currentLine[currentIndex+firstNonSpace+2] == 0x0061 && currentLine[currentIndex+firstNonSpace+3] == 0x0070 && currentLine[currentIndex+firstNonSpace+4] == 0x0074 && currentLine[currentIndex+firstNonSpace+5] == 0x0065 && currentLine[currentIndex+firstNonSpace+6] == 0x0072 && currentLine[currentIndex+firstNonSpace+7] == 0x0020 { // 'C'||'c', 'h', 'a', 'p', 't', 'e', 'r', ' '
 			return true
 		}
 		return false
@@ -242,14 +323,14 @@ class CueParser {
 		if currentLine.count < firstNonSpace+6 {
 			return false
 		}
-		if (currentLine[currentIndex+firstNonSpace] == 0x53 || currentLine[currentIndex+firstNonSpace] == 0x73) && currentLine[currentIndex+firstNonSpace+1] == 0x63 && currentLine[currentIndex+firstNonSpace+2] == 0x65 && currentLine[currentIndex+firstNonSpace+3] == 0x6e && currentLine[currentIndex+firstNonSpace+4] == 0x65 && currentLine[currentIndex+firstNonSpace+5] == 0x20 { // 'S'||'s', 'c', 'e', 'n', 'e', ' '
+		if (currentLine[currentIndex+firstNonSpace] == 0x0053 || currentLine[currentIndex+firstNonSpace] == 0x0073) && currentLine[currentIndex+firstNonSpace+1] == 0x0063 && currentLine[currentIndex+firstNonSpace+2] == 0x0065 && currentLine[currentIndex+firstNonSpace+3] == 0x006e && currentLine[currentIndex+firstNonSpace+4] == 0x0065 && currentLine[currentIndex+firstNonSpace+5] == 0x0020 { // 'S'||'s', 'c', 'e', 'n', 'e', ' '
 			return true
 		}
 		return false
 	}
 	
 	func scanDualPrefix() -> Bool {
-		if currentLine[currentIndex+firstNonSpace] == 0x5e {	// '^'
+		if currentLine[currentIndex+firstNonSpace] == 0x005e {	// '^'
 			return true
 		}
 		return false
@@ -259,7 +340,7 @@ class CueParser {
 		let saveOffset = offset
 		offset += 1
 		while offset < currentLine.count && (offset-saveOffset) < 24 {
-			if currentLine[currentIndex+firstNonSpace+offset] == 0x3a {	// ':'
+			if currentLine[currentIndex+firstNonSpace+offset] == 0x003a {	// ':'
 				return offset-saveOffset
 			}
 			offset += 1
