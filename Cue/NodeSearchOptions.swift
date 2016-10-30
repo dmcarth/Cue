6528094a69d4cd99ec53f51af29fb868a0f5128b
Dylan McArthur <dmcarth@users.noreply.github.com>
2016-10-29 20:52:59 -0700

    Added Objective-C compatibility

--- Cue/NodeSearchOptions.swift
+++ Cue/NodeSearchOptions.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2016 Dylan McArthur. All rights reserved.
 //
 
-public struct NodeSearchOptions {
-	// Enables search to traverse nodes deeper than level-1
+@objc public class NodeSearchOptions: NSObject {
+	
+	/// Enables search to traverse nodes deeper than level-1
 	public var deepSearchEnabled: Bool
-	// Search continues until the predicate returns true
+	
+	/// Causes search to return early when it finds a node matching this predicate. Search may not return a nodes matching this predicate if the given search index has no matching parent nodes.
 	public var predicate: (Node)->Bool
 	
 	public init(deepSearch: Bool = true, searchPredicate: ((Node)->Bool)?) {
