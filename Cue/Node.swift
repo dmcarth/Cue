e6ae708637fe822d18970e3c78bd3ae8f4d9b120
Dylan McArthur <dmcarth@users.noreply.github.com>
2016-12-14 03:22:55 -0700

    Moved block nodes to Block.swift

--- Cue/Node.swift
+++ Cue/Node.swift
@@ -53,14 +53,10 @@
 	
 }
 
-public class Block: Node {
-	public var lineNumber = 0
-}
+
 public class Inline: Node { }
 
-public class Document: Block {}
 
-public class Heading: Block {}
 public class Keyword: Inline {
 	public enum KeywordType {
 		case act
@@ -70,44 +66,6 @@ public class Keyword: Inline {
 	var type: KeywordType = .act
 }
 public class Identifier: Inline {}
-
-public class Description: Block {}
-public class CueBlock: Block {}
-public class LyricBlock: Block {}
-public class Lyric: Block {}
-public class CommentBlock: Block {}
-
-public class Cue: Block {
-	func addDefaultChildren(for results: [SearchResult]) {
-		let name = Name(startIndex: results[0].startIndex, endIndex: results[0].endIndex)
-		addChild(name)
-		
-		let del2 = Delimiter(startIndex: results[1].startIndex, endIndex: results[1].endIndex)
-		del2.type = .colon
-		addChild(del2)
-		
-		let te = RawText(startIndex: del2.endIndex, endIndex: endIndex)
-		addChild(te)
-	}
-}
-public class RegularCue: Cue {
-	override func addDefaultChildren(for results: [SearchResult]) {
-		addDefaultChildren(offset: results[0].startIndex)
-		
-		super.addDefaultChildren(for: results)
-	}
-}
-public class DualCue: Cue {
-	override func addDefaultChildren(for results: [SearchResult]) {
-		addDefaultChildren(offset: results[0].startIndex)
-		
-		let del = Delimiter(startIndex: results[0].startIndex, endIndex: results[0].endIndex)
-		del.type = .dual
-		addChild(del)
-		
-		super.addDefaultChildren(for: Array(results.dropFirst()))
-	}
-}
 
 public class RawText: Inline {}
 public class CommentText: Inline {}
