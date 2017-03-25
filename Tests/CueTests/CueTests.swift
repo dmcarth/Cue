dadb5fc863fc2fcc543cf39ceb6dc205878c230a
Dylan McArthur <dmcarth@users.noreply.github.com>
2017-03-25 06:09:06 -0700

    Reset to SwiftPM defaults

--- Tests/CueTests/CueTests.swift
+++ Tests/CueTests/CueTests.swift
@@ -2,22 +2,16 @@ import XCTest
 @testable import Cue
 
 class CueTests: XCTestCase {
-	
-    func testPerformanceExample() {
-		let fileURL = Bundle(for: CueTests.self).url(forResource: "war+peace", withExtension: "txt")!
-		var str = try! String(contentsOf: fileURL)
-		
-		let bytes = [UInt16](str.utf16)
-		
-		self.measure {
-			_ = Cue(input: bytes, codec: UTF16.self)
-		}
-	}
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(Cue().text, "Hello, World!")
+    }
+
 
     static var allTests : [(String, (CueTests) -> () throws -> Void)] {
         return [
-            ("testPerformanceExample", testPerformanceExample),
+            ("testExample", testExample),
         ]
     }
-	
 }
