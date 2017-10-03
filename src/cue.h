7b34bc7c46deed494b9880590ed2c8f9df2b140c
Dylan McArthur <dmcarth@users.noreply.github.com>
2017-10-02 22:20:13 -0700

    corrected documentation

--- src/cue.h
+++ src/cue.h
@@ -10,10 +10,11 @@
 
 typedef struct CueDocument CueDocument;
 
-/** Creates a CueParser from a UTF-8 encoded string `buff` of size `len`. It
- * is the client's responsibility to ensure `buff` is a valid UTF-8 string.
+/** Creates a CueDocument from a UTF-8 encoded string `utf8` of size `len`. It
+ * is the client's responsibility to ensure `utf8` is a valid UTF-8 string.
  */
-CueDocument *cue_document_from_utf8(const char *utf8, size_t len);
+CueDocument *cue_document_from_utf8(const char *utf8,
+									size_t len);
 
 void cue_document_free(CueDocument *doc);
 
